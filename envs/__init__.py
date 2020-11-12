1cba0e4f0a066a2086c9d4e6a35f81998b7840fb
Roxana Radulescu <8026679+rradules@users.noreply.github.com>
2020-11-12 17:09:45 +0100

    final cleanup

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,8 +1,4 @@
 from .act_game import ActGame
-from .prisoners_dilemma import IteratedPrisonersDilemma
-from .matching_pennies import IteratedMatchingPennies
 
 # Aliases
 IAG = ActGame
-IPD = IteratedPrisonersDilemma
-IMP = IteratedMatchingPennies
